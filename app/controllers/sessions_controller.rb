db57bed793fb1da17e940e1352d9e2e389d5f50b
hiroki <hiroki1995617@gmail.com>
2019-11-12 22:15:17 +0900

    Add user microposts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      #if user.activated? 認証済みユーザ判別の部分を暫定的に排除
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
-      else
+      #else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
         flash[:warning] = message
-        redirect_to root_url
-      end
+        #redirect_to root_url
+      #end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
